5671c626e0436d490940976941c7c62087a9f909
martin <martin.ming@students.ffhs.ch>
2024-10-06 20:17:19 +0200

    erweiterungen erstellt

--- src/main.cpp
+++ src/main.cpp
@@ -2,51 +2,169 @@
 #include <mxnFeatherBoard.h>
 
 
+uint8_t   state         = 0;
+uint16_t  numberDisplay = 0;
+uint8_t   ledPoint        = 1;
+bool      ledPointDirection = 0;
+
+void showLEDBar();
+void showKnightRider();
+void switchToLed();
+void printNumber(uint16_t numberToPrint);
+
 void setup() {
-  pinMode(LED1, OUTPUT);    // sets the digital pin 13 as output
-  pinMode(LED2, OUTPUT);    // sets the digital pin 13 as output
-  pinMode(LED3, OUTPUT);    // sets the digital pin 13 as output
-  pinMode(LED4, OUTPUT);    // sets the digital pin 13 as output
-  pinMode(LED5, OUTPUT);    // sets the digital pin 13 as output
-  pinMode(LED6, OUTPUT);    // sets the digital pin 13 as output
-  pinMode(LED7, OUTPUT);    // sets the digital pin 13 as output
-  pinMode(LED8, OUTPUT);    // sets the digital pin 13 as output
+  // sets the digital pins for LED's as output
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
+  pinMode(LED7, OUTPUT);
+  pinMode(LED8, OUTPUT);
+
+  // sets the digital pins for switch's as input
+  pinMode(SWITCH1, INPUT);
+  pinMode(SWITCH2, INPUT);
+  pinMode(SWITCH3, INPUT);
+
+  // sets the digital pins for 7-Segment Display's as output
+  pinMode(SegmentBitA, OUTPUT);
+  pinMode(SegmentBitB, OUTPUT);
+  pinMode(SegmentBitC, OUTPUT);
+  pinMode(SegmentBitD, OUTPUT);
+  pinMode(SegmentLD1, OUTPUT);    // read enable for 10er
+  pinMode(SegmentLD2, OUTPUT);    // read enable for 1er
+  pinMode(SegmentCC, OUTPUT);     // enapble the power for 7-Segment Display
+  
+  // sets the digital pin for the Power of 7-Segment Display ON
+  digitalWrite(SegmentCC, HIGH);
+
 }
 
 void loop() {
-      digitalWrite(LED1, HIGH); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED2, HIGH); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED3, HIGH); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED4, HIGH); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED5, HIGH); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED6, HIGH); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED7, HIGH); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED8, HIGH); // sets the digital pin 13 on
 
-      delay(500);            // waits for a second
+  //switchToLed();
 
-      digitalWrite(LED1, LOW);  // sets the digital pin 13 off
-      delay(100);            // waits for a second
-      digitalWrite(LED2, LOW); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED3, LOW); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED4, LOW); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED5, LOW);  // sets the digital pin 13 off
-      delay(100);            // waits for a second
-      digitalWrite(LED6, LOW); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED7, LOW); // sets the digital pin 13 on
-      delay(100);            // waits for a second
-      digitalWrite(LED8, LOW); // sets the digital pin 13 on
+  //check the state of switches
+  state = !digitalRead(SWITCH1) + (!digitalRead(SWITCH2) * 2) +  (!digitalRead(SWITCH3) * 4);
 
-      delay(500);            // waits for a second
+
+  switch(state){
+    case 0:
+      showLEDBar();
+      break;
+    case 1:  
+      printNumber(numberDisplay);
+      delay(100);
+      if(numberDisplay <99){
+        numberDisplay++;
+      }else{
+        numberDisplay = 0;
+      }
+      break;
+    case 2:   
+      showKnightRider();
+      break;
+  }
+
+}
+
+void showLEDBar(){
+  digitalWrite(LED1, HIGH); // sets the LED1 on
+  delay(100);            // waits for 0.1s
+  digitalWrite(LED2, HIGH);
+  delay(100);
+  digitalWrite(LED3, HIGH);
+  delay(100);
+  digitalWrite(LED4, HIGH);
+  delay(100);
+  digitalWrite(LED5, HIGH);
+  delay(100);
+  digitalWrite(LED6, HIGH);
+  delay(100);
+  digitalWrite(LED7, HIGH);
+  delay(100);
+  digitalWrite(LED8, HIGH);
+
+  delay(500);            // waits for a half second
+
+  digitalWrite(LED1, LOW);
+  delay(100);
+  digitalWrite(LED2, LOW);
+  delay(100);
+  digitalWrite(LED3, LOW);
+  delay(100);
+  digitalWrite(LED4, LOW);
+  delay(100);
+  digitalWrite(LED5, LOW);
+  delay(100);
+  digitalWrite(LED6, LOW);
+  delay(100);
+  digitalWrite(LED7, LOW);
+  delay(100);
+  digitalWrite(LED8, LOW);
+
+  delay(500);            // waits for a half second
+}
+
+void showKnightRider(){
+  digitalWrite(LED1, (ledPoint & BIT0));
+  digitalWrite(LED2, (ledPoint & BIT1));
+  digitalWrite(LED3, (ledPoint & BIT2));
+  digitalWrite(LED4, (ledPoint & BIT3));
+  digitalWrite(LED5, (ledPoint & BIT4));
+  digitalWrite(LED6, (ledPoint & BIT5));
+  digitalWrite(LED7, (ledPoint & BIT6));
+  digitalWrite(LED8, (ledPoint & BIT7));
+
+  if(ledPoint == 0b10000000){
+    ledPointDirection = 0;
+  }else if(ledPoint == 0b00000001){
+    ledPointDirection = 1;
+  }
+
+  if(ledPointDirection){
+    ledPoint = ledPoint << 1;
+  }else{
+    ledPoint = ledPoint >> 1;
+  }
+  delay(100);  
+}
+
+void switchToLed(){
+  if(digitalRead(SWITCH1)==1){
+    digitalWrite(LED1, HIGH);
+  }else{
+    digitalWrite(LED1, LOW);
+  }
+  if(digitalRead(SWITCH2)==1){
+    digitalWrite(LED2, HIGH);
+  }else{
+    digitalWrite(LED2, LOW);
+  }
+  if(digitalRead(SWITCH3)==1){
+    digitalWrite(LED3, HIGH);
+  }else{
+    digitalWrite(LED3, LOW);
+  }
+}
+
+
+void printNumber(uint16_t numberToPrint){
+  uint16_t numberToPrint10er = numberToPrint / 10;
+  uint16_t numberToPrint1er = numberToPrint % 10;
+  digitalWrite(SegmentLD1, HIGH);
+  digitalWrite(SegmentBitA, (numberToPrint10er & BIT0));
+  digitalWrite(SegmentBitB, (numberToPrint10er & BIT1));
+  digitalWrite(SegmentBitC, (numberToPrint10er & BIT2));
+  digitalWrite(SegmentBitD, (numberToPrint10er & BIT3));
+  digitalWrite(SegmentLD1, LOW);
+  
+  digitalWrite(SegmentLD2, HIGH);
+  digitalWrite(SegmentBitA, (numberToPrint1er & BIT0));
+  digitalWrite(SegmentBitB, (numberToPrint1er & BIT1));
+  digitalWrite(SegmentBitC, (numberToPrint1er & BIT2));
+  digitalWrite(SegmentBitD, (numberToPrint1er & BIT3));
+  digitalWrite(SegmentLD2, LOW);
 }
--- src/mxnFeatherBoard.h
+++ src/mxnFeatherBoard.h
@@ -6,6 +6,18 @@
 #define LED5        9
 #define LED6        6
 #define LED7        5
-#define LED8        4
+#define LED8        39
+
+#define SWITCH1        38
+#define SWITCH2        37
+#define SWITCH3        35
+
+#define SegmentBitA     18
+#define SegmentBitB     16
+#define SegmentBitC     15
+#define SegmentBitD     17
+#define SegmentLD1      14
+#define SegmentLD2      8
+#define SegmentCC       36
 
 int LEDs[8] = {LED1, LED2, LED3, LED4, LED5, LED6, LED7, LED8};
